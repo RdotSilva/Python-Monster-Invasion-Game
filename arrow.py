5f4701de760f9f1a41a75d1b1337c94b62c3ba07
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 12:22:12 -0400

    Add init method for arrow

---
+++ arrow.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Arrow(Sprite):
+    # Class to manage arrows shot by archer.
+
+    def __init__(self, mi_game):
+        # Create an arrow object at the archer's current position.
+        super().__init__()
+        self.screen = mi_game.screen
+        self.settings = mi_game.settings
+        self.color = self.settings.arrow_color
+
+        # Create an arrow react at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, self.settings.arrow_width, self.settings.arrow_height)
+        self.rect.midtop = mi_game.archer.rect.midtop
+
+        # Store the arrow's position as decimal value.
+        self.y = float(self.rect.y)
