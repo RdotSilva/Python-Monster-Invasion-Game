29189b72013a276c091bfc0bcd72c97819ca5a6a
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 11:59:02 -0400

    Add logic to increase archer move speed

--- archer.py
+++ archer.py
@@ -6,6 +6,7 @@ class Archer:
     def __init__(self, mi_game):
         # Initialize archer and set its starting position.
         self.screen = mi_game.screen
+        self.settings = mi_game.settings
         self.screen_rect = mi_game.screen.get_rect()
 
         # Load archer image and get its rect.
@@ -15,16 +16,23 @@ class Archer:
         # Start each new archer at bottom center of screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store decimal value for archer's horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         # Update archer position based on movement flag.
+        # Update archer x value (not the rect)
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.archer_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.archer_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         # Draw archer at its current location.
