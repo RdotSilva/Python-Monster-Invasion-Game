9b4dbe42dbdcf4384e6326a09177b3329942d252
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 10:52:33 -0400

    Add init method

--- archer.py
+++ archer.py
@@ -2,3 +2,15 @@ import pygame
 
 class Archer:
   # A class to manage the archer.
+
+  def __init__(self, ai_game):
+    # Initialize archer and set its starting position.
+    self.screen = ai_game.screen
+    self.screen_rect = ai_game.screen.get_rect()
+
+    # Load archer image and get its rect.
+    self.image = pygame.image.load('images/archer.bmp')
+    self.rect = self.image.get_rect()
+
+    # Start each new archer at bottom center of screen.
+    self.rect.modbottom = self.screen_rect.midbottom
