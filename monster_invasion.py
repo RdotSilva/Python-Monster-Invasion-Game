68293534c10d53571f470bf497e94be0584e6699
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 12:11:27 -0400

    Add _check_keyup_events method

--- monster_invasion.py
+++ monster_invasion.py
@@ -27,10 +27,7 @@ class MonsterInvasion:
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.archer.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.archer.moving_left = False
+                self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
         # Respond to keypresses.
@@ -39,6 +36,12 @@ class MonsterInvasion:
         elif event.key == pygame.K_LEFT:
             self.archer.moving_left = True
 
+    def _check_keyup_events(self, event):
+        # Response to key releases
+        if event.key == pygame.K_RIGHT:
+            self.archer.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.archer.moving_left = False
 
     def _update_screen(self):
         # Redraw the screen during each pass of the loop.
