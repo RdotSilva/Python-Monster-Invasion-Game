f94c899e8611bccbfbb4ee647552925039f266b7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 11:42:48 -0400

    Add update method

--- archer.py
+++ archer.py
@@ -1,23 +1,28 @@
 import pygame
 
 class Archer:
-  # A class to manage the archer.
+    # A class to manage the archer.
 
-  def __init__(self, mi_game):
-    # Initialize archer and set its starting position.
-    self.screen = mi_game.screen
-    self.screen_rect = mi_game.screen.get_rect()
+    def __init__(self, mi_game):
+        # Initialize archer and set its starting position.
+        self.screen = mi_game.screen
+        self.screen_rect = mi_game.screen.get_rect()
 
-    # Load archer image and get its rect.
-    self.image = pygame.image.load('images/archer.bmp')
-    self.rect = self.image.get_rect()
+        # Load archer image and get its rect.
+        self.image = pygame.image.load('images/archer.bmp')
+        self.rect = self.image.get_rect()
 
-    # Start each new archer at bottom center of screen.
-    self.rect.midbottom = self.screen_rect.midbottom
+        # Start each new archer at bottom center of screen.
+        self.rect.midbottom = self.screen_rect.midbottom
 
-    # Movement flag
-    self.moving_right = False
+        # Movement flag
+        self.moving_right = False
 
-  def blitme(self):
-    # Draw archer at its current location.
-    self.screen.blit(self.image, self.rect)
+    def update(self):
+        # Update archer position based on movement flag.
+        if self.moving_right:
+            self.rect.x += 1
+
+    def blitme(self):
+        # Draw archer at its current location.
+        self.screen.blit(self.image, self.rect)
