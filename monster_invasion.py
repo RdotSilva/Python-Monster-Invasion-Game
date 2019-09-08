fa29c2658690259a6c739cc9e2acfe0742009c5d
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 10:43:13 -0400

    Add run_game method

--- monster_invasion.py
+++ monster_invasion.py
@@ -13,3 +13,17 @@ def __init__(self):
 
     self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
     pygame.display.set_caption("Monster Invasion")
+
+def run_game(self):
+    # Start the main game loop.
+    while True:
+      # Watch for keyboard/mouse events.
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
+
+          # Redraw the screen during each pass of the loop.
+          self.screen.fill(self.bg_color)
+
+          # Make most recently drawn screen visible.
+          pygame.dispaly.flip()
