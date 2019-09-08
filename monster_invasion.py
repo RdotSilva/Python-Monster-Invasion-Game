5cd1479eae992f3a3d0c6bc2b5974e7079fcdaf7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 10:43:00 -0400

    Add init method

--- monster_invasion.py
+++ monster_invasion.py
@@ -6,4 +6,10 @@ import pygame
 class MonsterInvasion:
   # Class for game assets/behavior.
 
-  
+def __init__(self):
+    # Init the game & create resources.
+    pygame.init()
+    self.settings = Settings()
+
+    self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+    pygame.display.set_caption("Monster Invasion")
