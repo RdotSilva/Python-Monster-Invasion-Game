2a25182f83976511fda5a58564b00b7fb8891ae6
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 10:44:08 -0400

    Fix indentation

--- monster_invasion.py
+++ monster_invasion.py
@@ -6,27 +6,27 @@ import pygame
 class MonsterInvasion:
   # Class for game assets/behavior.
 
-def __init__(self):
-    # Init the game & create resources.
-    pygame.init()
-    self.settings = Settings()
+  def __init__(self):
+      # Init the game & create resources.
+      pygame.init()
+      self.settings = Settings()
 
-    self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-    pygame.display.set_caption("Monster Invasion")
+      self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+      pygame.display.set_caption("Monster Invasion")
 
-def run_game(self):
-    # Start the main game loop.
-    while True:
-      # Watch for keyboard/mouse events.
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit()
+  def run_game(self):
+      # Start the main game loop.
+      while True:
+        # Watch for keyboard/mouse events.
+        for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+            sys.exit()
 
-          # Redraw the screen during each pass of the loop.
-          self.screen.fill(self.bg_color)
+            # Redraw the screen during each pass of the loop.
+            self.screen.fill(self.bg_color)
 
-          # Make most recently drawn screen visible.
-          pygame.dispaly.flip()
+            # Make most recently drawn screen visible.
+            pygame.dispaly.flip()
 
 if __name__ == '__main__':
   # Make a game instance & run the game.
