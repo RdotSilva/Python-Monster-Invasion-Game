a167b2ed20b20365030374ce0b190ffcdc259c10
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 12:07:56 -0400

    Add logic to stop archer from going off screen

--- archer.py
+++ archer.py
@@ -26,9 +26,9 @@ class Archer:
     def update(self):
         # Update archer position based on movement flag.
         # Update archer x value (not the rect)
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.archer_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.archer_speed
 
         # Update rect object from self.x.
