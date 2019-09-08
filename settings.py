2c8ca29f8f829cd520ba903986ca9f019ce01c1a
Ryan Silva <RyandotSilva@gmail.com>
2019-09-08 10:44:35 -0400

    Add screen settings

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+  # Stores all of the settings for the game.
+
+  def __init__(self):
+    # Initialize settings
+
+    # Screen settings
+    self.screen_width = 1200
+    self.screen_height = 800
+    self.bg_color = (230, 230, 230)
