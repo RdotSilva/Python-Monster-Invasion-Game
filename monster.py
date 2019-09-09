3f4849eb7d55b763ca61b2d1f0fafbc785c03f35
Ryan Silva <RyandotSilva@gmail.com>
2019-09-09 16:49:27 -0400

    Add monster image to init method

--- monster.py
+++ monster.py
@@ -8,3 +8,7 @@ class Monster(Sprite):
         # Initialize the monster and set its starting position.
         super().__init__()
         self.screen = mi_game.screen
+
+        # Load the monster image and set its rect attribute.
+        self.image = pygame.image.load('/images/monster.bmp')
+        self.rect = self.image.get_rect()
