d032fbca143b1d06f7a1636eaef971b5c98cbbbd
Ryan Silva <RyandotSilva@gmail.com>
2019-09-09 16:48:41 -0400

    Add init method

--- monster.py
+++ monster.py
@@ -2,4 +2,9 @@ import pygame
 from pygame.sprite import Sprite
 
 class Monster(Sprite):
-    
+    # Class to represent a single monster.
+
+    def __init__(self, mi_game):
+        # Initialize the monster and set its starting position.
+        super().__init__()
+        self.screen = mi_game.screen
