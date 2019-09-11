b8cd53e1bc70017095d3fbf46e982a7325c71faa
Ryan Silva <RyandotSilva@gmail.com>
2019-09-11 19:00:30 -0400

    Add init method

--- game_stats.py
+++ game_stats.py
@@ -1,4 +1,7 @@
 class GameStats:
     # Track stats for Monster Invasion Game.
 
-    
+    def __init__(self, mi_game):
+        # Init stats
+        self.settings = mi_game.settings
+        self.reset_stats()
