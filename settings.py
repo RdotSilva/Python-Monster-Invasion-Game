e67da4abbd6e83204386a012eb5bbf6cb2ff3c0f
Ryan Silva <RyandotSilva@gmail.com>
2019-09-11 18:54:04 -0400

    Update arrow speed for testing

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ class Settings:
         self.archer_speed = 1.5
 
         # Arrow settings
-        self.arrow_speed = 1.0
+        self.arrow_speed = 1.5
         self.arrow_width = 300
         self.arrow_height = 15
         self.arrow_color = (60, 60, 60)
