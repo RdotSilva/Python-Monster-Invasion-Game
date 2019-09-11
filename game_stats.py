24a4c1a1961b9d49a48d10a9ddab7f3ab8fdb5fd
Ryan Silva <RyandotSilva@gmail.com>
2019-09-11 19:01:04 -0400

    Add reset_stats method

--- game_stats.py
+++ game_stats.py
@@ -5,3 +5,7 @@ class GameStats:
         # Init stats
         self.settings = mi_game.settings
         self.reset_stats()
+
+    def reset_stats(self):
+        # Init stats that can change during the game.
+        self.lives_left = self.settings.life_limit
