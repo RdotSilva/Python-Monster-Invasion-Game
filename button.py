e91b5db62e8959d8035389b7d1eb6cf8a92803c5
Ryan Silva <RyandotSilva@gmail.com>
2019-09-13 18:12:28 -0400

    Add button dimensions & properties

--- button.py
+++ button.py
@@ -7,4 +7,9 @@ class Button:
         self.screen = mi_game.screen
         self.screen_rect = self.screen.get_rect()
 
-        
+        # Set dimensions & properties of button.
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
