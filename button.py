f91936f7a10b299fc7d91105ba767731b30b2b8d
Ryan Silva <RyandotSilva@gmail.com>
2019-09-13 18:11:51 -0400

    Add init method

--- button.py
+++ button.py
@@ -1,4 +1,10 @@
 import pygame.font
 
 class Button:
-    
+
+    def __init__(self, mi_game, msg):
+        # Init button attributes.
+        self.screen = mi_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        
