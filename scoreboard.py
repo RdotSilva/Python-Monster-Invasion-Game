546009630045e864d9a688409519dae3de9261a9
Ryan Silva <RyandotSilva@gmail.com>
2019-09-14 18:36:28 -0400

    Add score display to prep_score method

--- scoreboard.py
+++ scoreboard.py
@@ -21,3 +21,8 @@ class Scoreboard:
         # Turn the score into a rendered image.
         score_str = str(self.stats.score)
         self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+
+        # Display the score at top right of screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
