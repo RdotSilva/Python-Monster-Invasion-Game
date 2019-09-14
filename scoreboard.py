1d1722bbb060d56813f09f2c6c54cc2aece72ae1
Ryan Silva <RyandotSilva@gmail.com>
2019-09-14 18:33:19 -0400

    Add init method

--- scoreboard.py
+++ scoreboard.py
@@ -2,4 +2,11 @@ import pygame.font
 
 class Scoreboard:
     # Class to report scoring info.
-    
+
+    def __init__(self, mi_game):
+        # Initialize scorekeeping attributes.
+        self.screen = mi_game.screen
+        self.screen_rect = self.screen.get_rect()
+        self.settings = mi_game.settings
+        self.stats = mi_game.stats
+        
