c95372c92d3400a43c89b002448d1466e4fc6ea7
Ryan Silva <RyandotSilva@gmail.com>
2019-09-14 18:17:22 -0400

    Update play button to reset settings on new game

--- monster_invasion.py
+++ monster_invasion.py
@@ -114,6 +114,9 @@ class MonsterInvasion:
         # Start a new game when player clicks Play button.
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
+            
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
