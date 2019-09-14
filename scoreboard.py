c711f20a4b6404951bfb31122cd0658e98d26382
Ryan Silva <RyandotSilva@gmail.com>
2019-09-14 18:34:01 -0400

    Add font settings to init method

--- scoreboard.py
+++ scoreboard.py
@@ -9,4 +9,10 @@ class Scoreboard:
         self.screen_rect = self.screen.get_rect()
         self.settings = mi_game.settings
         self.stats = mi_game.stats
-        
+
+        # Font settings for scoring info.
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # prepare initial score image.
+        self.prep_score()
