dbb555a4ee26ab07195b5bd8be6b94602f264401
Ryan Silva <RyandotSilva@gmail.com>
2019-09-16 16:21:05 -0400

    Update prep high score with centering of score

--- scoreboard.py
+++ scoreboard.py
@@ -24,6 +24,11 @@ class Scoreboard:
         high_score_str ="{:,}".format(high_score)
         self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
+        # Center the high score at top of screen.
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+        
     def prep_score(self):
         # Turn the score into a rendered image.
         rounded_score = round(self.stats.score, -1)
