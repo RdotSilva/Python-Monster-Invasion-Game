f53c3f2e411bbbf8f5e4dd8332c1a4b8073e775c
Ryan Silva <RyandotSilva@gmail.com>
2019-09-16 16:19:39 -0400

    Add prep_high_score method

--- scoreboard.py
+++ scoreboard.py
@@ -16,6 +16,13 @@ class Scoreboard:
 
         # Prepare initial score image.
         self.prep_score()
+        self.prep_high_score()
+
+    def prep_high_score(self):
+        # Turn the high score into a rendered image.
+        high_score = round(self.stats.high_score, -1)
+        high_score_str ="{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
 
     def prep_score(self):
         # Turn the score into a rendered image.
