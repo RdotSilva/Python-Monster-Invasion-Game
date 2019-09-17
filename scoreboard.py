e0beb8bfc5880538881090f07b6a9f12b21baceb
Ryan Silva <RyandotSilva@gmail.com>
2019-09-17 17:56:07 -0400

    Update show_score to draw archer lives left

--- scoreboard.py
+++ scoreboard.py
@@ -48,6 +48,7 @@ class Scoreboard:
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.archers.draw(self.screen)
 
     def check_high_score(self):
         # Check to see if there's a new high score.
