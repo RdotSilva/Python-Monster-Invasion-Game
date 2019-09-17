5650cbf099a20157e8d8e7e77e22ee867449ff07
Ryan Silva <RyandotSilva@gmail.com>
2019-09-17 17:43:11 -0400

    Update show_score to show level as well

--- scoreboard.py
+++ scoreboard.py
@@ -45,6 +45,7 @@ class Scoreboard:
         # Draw score to screen.
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
 
     def check_high_score(self):
         # Check to see if there's a new high score.
